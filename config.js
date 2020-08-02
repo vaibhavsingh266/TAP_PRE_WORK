f6616d60d8095db4f4f90cd9495a7d80c203c1a7
vaibhavsingh266 <61136047+vaibhavsingh266@users.noreply.github.com>
2020-08-02 20:19:04 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-2_j6EVawpVF', // e.g. us-east-2_uXboG5pAb
+        region: 'us-east-2', // e.g. us-east-2
+        clientId: '4n7og6o7c3ftv1lo23v97dvktf'//is this used anywhere?
+    },
+};
